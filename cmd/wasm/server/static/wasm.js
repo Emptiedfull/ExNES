08cb43f9b91cb58e2ddd74742973b96781217c10
emptiedfull <atharavagarwal11@gmail.com>
2026-06-14 16:51:01 +0530

    messing around with wasm

--- cmd/wasm/server/static/wasm.js
+++ cmd/wasm/server/static/wasm.js
@@ -1,5 +1,21 @@
 const go = new Go()
-WebAssembly.instantiateStreaming(fetch("/static/assests/emulator.wasm"),go.importObject).then((result)=>{
+WebAssembly.instantiateStreaming(fetch("/static/emulator.wasm"),go.importObject).then((result)=>{
     go.run(result.instance)
-    console.log("go wasm lloading")
+  
+})
+
+
+
+window.addEventListener("DOMContentLoaded",()=>{
+    console.log("loading")
+    btn = document.getElementById("start")
+    btn.addEventListener("click",()=>{
+        console.log("btn clicked")
+        console.log(startEmulator())
+    })
+
+    framebtn = document.getElementById("frame")
+    framebtn.addEventListener("click",()=>{
+        runFrame()
+    })
 })
